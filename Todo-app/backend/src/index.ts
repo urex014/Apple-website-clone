58e662eff9dcf64043c273e1620c415e87140e1a
Amarachukwu Onuoha <amarachukwuonuohabars007@gmail.com>
2025-03-25 23:11:12 +0100

    backend commit

--- Todo-app/backend/src/index.ts
+++ Todo-app/backend/src/index.ts
@@ -1,14 +1,19 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 import dotenv from 'dotenv';
 import todoRoutes from './routes/todo';
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/todos', todoRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => {
+    console.log(`🚀 Server running on http://localhost:${port}`);
+});
+export default app;
